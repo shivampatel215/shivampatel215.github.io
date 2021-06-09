2c33eea6ef63643667d921c1e36ec075928672be
shivampatel215 <shivampatel215@berkeley.edu>
2021-06-09 15:01:56 -0700

    commit

---
+++ src/Navbar.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import './navbar.scss'
+import resume from "./data/Shivam Resume 2021.pdf"
+import{Link} from "react-scroll";
+// import { motion } from "framer-motion";
+import {useState} from 'react';
+// import {NavLink, BrowserRouter as Router} from 'react-router-dom';
+
+
+function Navbar({toggler}) {
+
+
+    const [clicked, setClicked] = useState(false);
+
+    return (
+      <div>
+          <div className="navbar--mobile">
+            <div className="navbar--mobile--menuicon">
+              <span
+              className={clicked ? "fas fa-times" : "fas fa-bars"}
+              onClick={() => setClicked(!clicked)}>
+              </span>
+            </div>
+            <h1><a className="logo" href="/"><span>Shivam</span><span>Patel</span></a></h1>
+            <input id="toggle" class="toggle" type="checkbox" onClick={toggler}></input>
+
+
+          </div>
+          <div className={clicked ? "navbar--mobile_menu open" : "navbar--mobile_menu"}
+          onClick={() => setClicked(!clicked)}>
+            <ul className="links">
+                <Link
+            activeClass="active"
+            to="portfolio"
+            spy={true}
+            offset={-30}
+            smooth={true}
+            duration={500}
+            onClick={() => setClicked(!clicked)}
+          >
+            <li>
+              portfolio
+
+            </li>
+          </Link>
+          <Link
+            activeClass="active"
+            to="about-me-app"
+            spy={true}
+            offset={-30}
+            smooth={true}
+            duration={500}
+            onClick={() => setClicked(!clicked)}
+          >
+            <li>
+              about me
+            </li>
+          </Link>
+
+            <li>
+              <a className="resume-button"
+                href={resume}
+                target="_blank" rel="norreferrer">
+                resume
+              </a>
+            </li>
+        
+
+                </ul>
+
+
+          </div>
+
+          
+          <div className="navbar">
+            <div className="navbar-left">
+              <h1><a className="logo" href="/"><span>Shivam</span><span>Patel</span></a></h1>
+            </div>
+            <div className="navbar-right">
+                <ul className="links">
+                <Link
+            activeClass="active"
+            to="portfolio"
+            spy={true}
+            offset={-30}
+            smooth={true}
+            duration={500}
+            onClick={() => setClicked(!clicked)}
+          >
+            <li>
+              portfolio
+
+            </li>
+          </Link>
+          <Link
+            activeClass="active"
+            to="about-me-app"
+            spy={true}
+            offset={-30}
+            smooth={true}
+            duration={500}
+            onClick={() => setClicked(!clicked)}
+          >
+            <li>
+              about me
+            </li>
+          </Link>
+
+            <li>
+              <a className="resume-button"
+                href={resume}
+                target="_blank" rel="norreferrer">
+                resume
+              </a>
+            </li>
+        
+
+                </ul>
+                <input id="toggle" class="toggle" type="checkbox" onClick={toggler}></input>
+                {/* <button onClick={toggler}>dark</button> */}
+            </div>
+        </div>
+      </div>
+      
+    )
+}
+
+export default Navbar;
---
+++ src/card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './card.scss';
+// import mewtwo from './data/images/mewtoo.png';
+import {portfolioData} from './data/constants.js';
+
+function card(props) {
+    return (
+    
+            <div class="container">
+                <div class="card">
+                    <div class="imgBx">
+                    <img src={`${process.env.PUBLIC_URL}/images/${props.imgsrc}`} />
+                    </div>
+                    <div class="contentBx">
+                    <h2>{props.title}</h2>
+                    <div class="size">
+                        <h3>{props.description}</h3>
+                    </div>
+                    <a target="_blank" href={props.websitelink}>Check Out</a>
+                    <a class="git" href={props.githublink} target="_blank" aria-label="GitHub">
+                    <i class="fab fa-github grow" ></i></a>
+                    </div>
+                </div>
+            </div>
+
+            
+        
+    )
+}
+
+export default card
